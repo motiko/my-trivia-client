718e8e49a678d5ffbdac9c09530b4c5fe43a2c67
moti <moti@practitest.com>
2017-04-16 08:16:12 +0700

    small fix

--- app/routes/admin/question/new.js
+++ app/routes/admin/question/new.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   model() {
     return Ember.Object.create({
       text : "",
-      answers: Array(4).fill().map(() => (Ember.Object.create({text:"123",correct:false})))
+      answers: Array(4).fill().map((_, i) => (Ember.Object.create({text:`Answer ${i+1}`,correct:false})))
     })
   },
   actions: {
